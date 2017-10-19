48cd68f9843112620886595057869815e51eb924
Francesca <francesca@Serenity-Mobile>
2017-10-19 12:05:12 -0400

    Fixed graph class errors, compiles successfully. Commented out Vertex class in favor of a std::pair<int, bool>

--- src/cvers/graph.cpp
+++ src/cvers/graph.cpp
@@ -1,17 +1,28 @@
-#include <algorithm> //Replace, remove
+#include <algorithm> //Replace, remove, find
 #include <iostream>
 #include "graph.h"
 
 using std::remove;
+using std::find;
 using std::get;
 using std::cout;
 
-Vertex::Vertex(int label, bool visited) {
-  this->label = label;
-  this->visited = visited;
-}
+struct CompareFirst
+{
+  CompareFirst(int val) : val_(val) {}
+  bool operator()(const std::pair<int,int>& elem) const {
+    return val_ == elem.first;
+  }
+  private:
+    int val_;
+};
 
-Graph::Graph (vector< Vertex > vertices, vector<tuple<int, int, int> > edges,
+// Vertex::Vertex(int label, bool visited) {
+//   this->label = label;
+//   this->visited = visited;
+// }
+
+Graph::Graph (vector< pair<int, bool>  > vertices, vector<tuple<int, int, int> > edges,
   vector< unordered_set<int> > adjacencies){
 
   this->vertices = vertices;
@@ -20,24 +31,24 @@ Graph::Graph (vector< Vertex > vertices, vector<tuple<int, int, int> > edges,
   update_adjacencies();
 }
 
-void Graph::add_vertex(Vertex vertex){
+void Graph::add_vertex(pair<int, bool>  vertex){
   vertices.push_back(vertex);
 }
 
-void Graph::remove_vertex(Vertex vertex){
+void Graph::remove_vertex(pair<int, bool>  vertex){
   vertices.erase(remove(vertices.begin(), vertices.end(), vertex), vertices.end());
   vector<int> remove_these;
 
   size_t i;
   for (i = 0; i < edges.size(); i++){
-      if (get<0>(edges[i]) == vertex.label || get<1>(edges[i]) == vertex.label){
+      if (get<0>(edges[i]) == vertex.first || get<1>(edges[i]) == vertex.first){
         vertices.erase(vertices.begin() + i);
         i--; //edges will be one element smaller, this index will be different now
       }
   }
 
   for (i = 0; i < adjacencies.size(); i++){
-    if ((adjacencies[i]).find(vertex.label) != adjacencies.end()){
+    if ((adjacencies[i]).find(vertex.first) != (adjacencies[i]).end()){
       adjacencies.erase(adjacencies.begin() + i);
       i--; //ditto
     }
@@ -45,13 +56,13 @@ void Graph::remove_vertex(Vertex vertex){
 }
 
 void Graph::add_edge(int source, int dest, int weight){
-  if ((vertices.begin(), vertices.end(), source) == vertices.end()) {
-    Vertex vsrc = Vertex(source, false);
+  if (find_if(vertices.begin(),vertices.end(), CompareFirst(source)) == vertices.end()) {
+    pair<int, bool>  vsrc(source, false);
     vertices.push_back(vsrc);
   }
 
-  if ((vertices.begin(), vertices.end(), dest) == vertices.end()) {
-    Vertex vdest = Vertex(dest, false);
+  if (find_if(vertices.begin(),vertices.end(), CompareFirst(dest)) == vertices.end()) {
+    pair<int, bool> vdest(dest, false);
     vertices.push_back(vdest);
   }
 
@@ -62,7 +73,7 @@ void Graph::add_edge(int source, int dest, int weight){
   adjacencies.push_back(new_adjacency);
 }
 
-bool has_edge(int vert1, int vert2){
+bool Graph::has_edge(int vert1, int vert2){
   unordered_set<int> edge ( {vert1, vert2} );
   if (find(adjacencies.begin(), adjacencies.end(), edge) == adjacencies.end())
     return false;
@@ -73,15 +84,15 @@ bool has_edge(int vert1, int vert2){
 void Graph::update_adjacencies() {
   for (size_t i = 0; i < edges.size(); i++){
     unordered_set<int> edge ({get<0>(edges[i]), get<1>(edges[i])});
-    edges.push_back(edge);
+    adjacencies.push_back(edge);
   }
 }
 
-void Graph::print_graph() {
-  cout << "Begin graph" << std::endl;
-  cout << "-------------------------------\n";
-  cout << "Vertices: " << vertices << std::endl;
-  cout << "Edges: " << edges << std::endl;
-  cout << "-------------------------------\n";
-  cout << "End graph" << std::endl;
-}
+// void Graph::print_graph() {
+//   cout << "Begin graph" << std::endl;
+//   cout << "-------------------------------\n";
+//   cout << "Vertices: " << vertices << std::endl;
+//   cout << "Edges: " << edges << std::endl;
+//   cout << "-------------------------------\n";
+//   cout << "End graph" << std::endl;
+// }
--- src/cvers/graph.h
+++ src/cvers/graph.h
@@ -9,22 +9,22 @@ using std::tuple;
 using std::unordered_set;
 using std::pair;
 
-class Vertex{
-public:
-  Vertex (int label, bool visited);
-  int label;
-  bool visited;
-};
+// class Vertex{
+// public:
+//   Vertex (int label, bool visited);
+//   int label;
+//   bool visited;
+// };
 
 class Graph{
 public:
-  vector < Vertex > vertices;
+  vector < pair<int, bool> > vertices;
   vector < tuple<int, int, int> > edges;
   vector < unordered_set<int> > adjacencies;
 
-  Graph (vector< Vertex > vertices, vector<tuple<int, int, int> > edges, vector<unordered_set<int> > adjacencies);
-  void add_vertex(Vertex vertex); //Vertices are currently represented by pairs.
-  void remove_vertex(Vertex vertex);
+  Graph (vector< pair<int, bool>  > vertices, vector<tuple<int, int, int> > edges, vector<unordered_set<int> > adjacencies);
+  void add_vertex(pair<int, bool>  vertex); //Vertices are currently represented by pairs.
+  void remove_vertex(pair<int, bool>  vertex);
 
   void add_edge(int source, int dest, int weight);
 
